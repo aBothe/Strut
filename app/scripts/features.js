bd07c480f36022d80ef0fd25714aeb85a91329c9
Alexander Bothe <info@alexanderbothe.com>
2014-12-11 16:26:25 +0100

    Enabled reveal support

--- app/scripts/features.js
+++ app/scripts/features.js
@@ -11,6 +11,7 @@ define([
 	'strut/importer/main',
 	'strut/exporter/main',
 	'strut/presentation_generator/impress/main',
+	'strut/presentation_generator/reveal/main',
 	'strut/presentation_generator/bespoke/main',
 	'strut/presentation_generator/handouts/main',
 	'strut/presentation_generator/main',
